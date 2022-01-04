25b2a63b18729d7dd49c40fa3992a00d53728fb7
Crowdbotics <engineering@crowdbotics.com>
2022-01-04 19:45:04 +0000

    Update models

--- backend/home/admin.py
+++ backend/home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import AnotherModel
+
+admin.site.register(AnotherModel)
 
 # Register your models here.
--- backend/home/models.py
+++ backend/home/models.py
@@ -1,1 +1,10 @@
+from django.conf import settings
 from django.db import models
+
+
+class AnotherModel(models.Model):
+    "Generated Model"
+    name = models.CharField(
+        max_length=256,
+    )
+    age = models.IntegerField()
